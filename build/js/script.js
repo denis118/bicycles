6ee6520a5bcd29cda53f928e7882a898219e7c4e
Denis Tsap <ts.dios.v@gmail.com>
2021-05-30 22:49:19 +0300

    Пишет JS для валидации формы и плавного скролла

--- build/js/script.js
+++ build/js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Показ и скрытие навигации
 (function () {
   var DESKTOP_WIDTH = 1024;
   var viewPort = document.documentElement.clientWidth;
@@ -35,7 +36,6 @@
     if (isEveryItemExisting && viewPort < DESKTOP_WIDTH) {
       hideNavigation();
       toggle.addEventListener('click', onToggleClick, false);
-      console.log('in');
     }
 
     if (isEveryItemExisting && (viewPort > DESKTOP_WIDTH || viewPort === DESKTOP_WIDTH)) {
@@ -60,11 +60,13 @@
     ].forEach(function (item) {
       return item.classList.remove('hidden-entity');
     });
+
     navigationList.classList.add('hidden-entity');
   }
 
   function showNavigation() {
     navigation.classList.remove('navigation--js');
+
     [
       toggle,
       burgerDescription,
@@ -72,6 +74,7 @@
     ].forEach(function (item) {
       return item.classList.add('hidden-entity');
     });
+
     navigationList.classList.remove('hidden-entity');
   }
 
@@ -84,38 +87,113 @@
         navigationList.classList.remove('hidden-entity');
         toggle.classList.add('navigation__toggle--cross');
         body.classList.add('modal-open');
+
         [
           burgerDescription,
           burgerSvg
         ].forEach(function (item) {
           return item.classList.add('hidden-entity');
         });
+
         [
           crossDescription,
           crossSvg
         ].forEach(function (item) {
           return item.classList.remove('hidden-entity');
         });
+
         flag = true;
       } else {
         navigation.classList.remove('navigation--opened');
         navigationList.classList.add('hidden-entity');
         toggle.classList.remove('navigation__toggle--cross');
         body.classList.remove('modal-open');
+
         [
           burgerDescription,
           burgerSvg
         ].forEach(function (item) {
           return item.classList.remove('hidden-entity');
         });
+
         [
           crossDescription,
           crossSvg
         ].forEach(function (item) {
           return item.classList.add('hidden-entity');
         });
+
         flag = false;
       }
     };
+  }
+
+  Window.navigationSwitching = {
+    desktopWidth: DESKTOP_WIDTH,
+    hideNavigation: onToggleClick
+  };
+})();
+
+// Валидация формы
+(function () {
+  var generalBlock = document.querySelector('.general-block');
+  var form = generalBlock.querySelector('form');
+  var phoneInput = form.querySelector('input[type="tel"]');
+  var submitButton = form.querySelector('[type="submit"]');
+
+  var checkedItems = [
+    phoneInput,
+    submitButton
+  ];
+
+  var isEveryItemExisting = checkedItems.every(function (item) {
+    return Boolean(item) === true;
+  });
+
+  if (isEveryItemExisting) {
+    submitButton.addEventListener('click', function (evt) {
+      var value = typeof phoneInput.value === 'string' ? phoneInput.value : String(phoneInput.value);
+      var isEveryCharLetter = value.trim().split('').every(function (char) {
+        return Boolean(Number(char));
+      });
+
+      if (!isEveryCharLetter || phoneInput.value === '') {
+        evt.preventDefault();
+        phoneInput.setCustomValidity('Номер телефона может содержать только цифры');
+        phoneInput.reportValidity();
+      } else {
+        phoneInput.setCustomValidity('');
+      }
+    });
+  }
+})();
+
+// Скролл
+(function () {
+  var navigationList = document.querySelector('.navigation__list');
+  navigationList.addEventListener('click', onNavigationListClick, false);
+
+  function onNavigationListClick(evt) {
+    evt.preventDefault();
+
+    var isNavigationLink = navigationList.contains(evt.target)
+      && Boolean(evt.target.getAttribute('href'));
+
+    if (isNavigationLink) {
+      var aimId = evt.target.getAttribute('href');
+      var aim = document.querySelector(aimId);
+
+      if (aim) {
+        if (document.documentElement.clientWidth
+            < Window.navigationSwitching.desktopWidth) {
+          Window.navigationSwitching.hideNavigation();
+        }
+
+        aim.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+      }
+    }
   }
 })();
