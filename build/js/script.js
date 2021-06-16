dfc2d7964da70b083b42e96ac0f9b59fd545a779
Denis Tsap <ts.dios.v@gmail.com>
2021-06-16 20:16:52 +0300

    Save

--- build/js/script.js
+++ build/js/script.js
@@ -13,6 +13,8 @@
   var crossSvg = navigation.querySelector('.navigation__cross-svg');
   var navigationList = navigation.querySelector('.navigation__list');
 
+  var flag = false;
+
   var checkedItems = [
     navigation,
     toggle,
@@ -27,9 +29,7 @@
     return Boolean(item) === true;
   });
 
-  var onToggleClick = createHandler();
-
-  window.addEventListener('load', function () {
+  document.addEventListener('DOMContentLoaded', function () {
     viewPort = document.documentElement.clientWidth;
 
     if (isEveryItemExisting && viewPort < DESKTOP_WIDTH) {
@@ -43,17 +43,22 @@
 
     if (isEveryItemExisting && viewPort < DESKTOP_WIDTH) {
       hideNavigation();
-      toggle.addEventListener('click', onToggleClick, false);
+      flag = false;
+      toggle.addEventListener('click', onToggleClick);
     }
 
     if (isEveryItemExisting && (viewPort > DESKTOP_WIDTH || viewPort === DESKTOP_WIDTH)) {
       showNavigation();
-      toggle.removeEventListener('click', onToggleClick, false);
+      toggle.removeEventListener('click', onToggleClick);
     }
   });
 
   function hideNavigation() {
     navigation.classList.add('navigation--js');
+    navigation.classList.remove('navigation--opened');
+    toggle.classList.remove('navigation__toggle--cross');
+    body.classList.remove('modal-open');
+
     [
       toggle,
       burgerDescription,
@@ -62,11 +67,18 @@
       return item.classList.remove('hidden-entity');
     });
 
-    navigationList.classList.add('hidden-entity');
+    [
+      crossDescription,
+      crossSvg,
+      navigationList
+    ].forEach(function (item) {
+      return item.classList.add('hidden-entity');
+    });
   }
 
   function showNavigation() {
     navigation.classList.remove('navigation--js');
+    navigation.classList.add('navigation--opened');
 
     [
       toggle,
@@ -76,57 +88,59 @@
       return item.classList.add('hidden-entity');
     });
 
-    navigationList.classList.remove('hidden-entity');
+    [
+      crossDescription,
+      crossSvg,
+      navigationList
+    ].forEach(function (item) {
+      return item.classList.remove('hidden-entity');
+    });
   }
 
-  function createHandler() {
-    var flag = false;
+  function onToggleClick() {
+    if (!flag) {
+      navigation.classList.add('navigation--opened');
+      navigationList.classList.remove('hidden-entity');
+      toggle.classList.add('navigation__toggle--cross');
+      body.classList.add('modal-open');
 
-    return function () {
-      if (!flag) {
-        navigation.classList.add('navigation--opened');
-        navigationList.classList.remove('hidden-entity');
-        toggle.classList.add('navigation__toggle--cross');
-        body.classList.add('modal-open');
+      [
+        burgerDescription,
+        burgerSvg
+      ].forEach(function (item) {
+        return item.classList.add('hidden-entity');
+      });
 
-        [
-          burgerDescription,
-          burgerSvg
-        ].forEach(function (item) {
-          return item.classList.add('hidden-entity');
-        });
+      [
+        crossDescription,
+        crossSvg
+      ].forEach(function (item) {
+        return item.classList.remove('hidden-entity');
+      });
 
-        [
-          crossDescription,
-          crossSvg
-        ].forEach(function (item) {
-          return item.classList.remove('hidden-entity');
-        });
+      flag = true;
+    } else {
+      navigation.classList.remove('navigation--opened');
+      navigationList.classList.add('hidden-entity');
+      toggle.classList.remove('navigation__toggle--cross');
+      body.classList.remove('modal-open');
 
-        flag = true;
-      } else {
-        navigation.classList.remove('navigation--opened');
-        navigationList.classList.add('hidden-entity');
-        toggle.classList.remove('navigation__toggle--cross');
-        body.classList.remove('modal-open');
+      [
+        burgerDescription,
+        burgerSvg
+      ].forEach(function (item) {
+        return item.classList.remove('hidden-entity');
+      });
 
-        [
-          burgerDescription,
-          burgerSvg
-        ].forEach(function (item) {
-          return item.classList.remove('hidden-entity');
-        });
+      [
+        crossDescription,
+        crossSvg
+      ].forEach(function (item) {
+        return item.classList.add('hidden-entity');
+      });
 
-        [
-          crossDescription,
-          crossSvg
-        ].forEach(function (item) {
-          return item.classList.add('hidden-entity');
-        });
-
-        flag = false;
-      }
-    };
+      flag = false;
+    }
   }
 
   window.navigationSwitching = {
